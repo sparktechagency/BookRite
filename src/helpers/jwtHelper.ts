de81cf31b6517f3ac1e62ac305a5c35a4dde4904
azizulhoq953 <azizulhoq2041@gmail.com>
2025-10-30 12:44:54 +0600

    update

--- src/helpers/jwtHelper.ts
+++ src/helpers/jwtHelper.ts
@@ -1,16 +1,5 @@
-// import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
-
-// const createToken = (payload: object, secret: Secret, expireTime: string) => {
-//   return jwt.sign(payload, secret, { expiresIn: expireTime });
-// };
-
-// const verifyToken = (token: string, secret: Secret) => {
-//   return jwt.verify(token, secret) as JwtPayload;
-// };
-
-// export const jwtHelper = { createToken, verifyToken };
 import jwt, { JwtPayload, Secret, TokenExpiredError } from 'jsonwebtoken';
-import  ApiError  from '../errors/ApiError';
+import ApiError from '../errors/ApiError';
 import { StatusCodes } from 'http-status-codes';
 
 // Create a new JWT token
